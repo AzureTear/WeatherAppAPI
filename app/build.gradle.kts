b617aa8bb2e13dc3d68e997314ca9969780fde04
AzureTear <ba.maiolatesi@duocuc.cl>
2025-11-23 20:40:17 -0300

    Created data Class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,11 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
-    
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.6")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
