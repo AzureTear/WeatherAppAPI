bf124152f7bf79edf880d2cf510b67e38d46d3d6
AzureTear <ba.maiolatesi@duocuc.cl>
2025-12-17 13:40:09 -0300

    Removed unnecessary files

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -19,26 +19,17 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.weatherapp.data.remote.AppDatabase
 import com.example.weatherapp.navigation.BottomBar
 import com.example.weatherapp.navigation.BottomNavItem
 import com.example.weatherapp.navigation.Routes
-import com.example.weatherapp.ui.screens.DetailScreen
-import com.example.weatherapp.ui.screens.HomeScreen
 import com.example.weatherapp.ui.screens.HomeScreen2
-import com.example.weatherapp.ui.screens.SavedDaoScreen
 import com.example.weatherapp.ui.theme.WeatherAppTheme
-import com.example.weatherapp.ui.viewmodel.ItemViewModel
-import com.example.weatherapp.ui.viewmodel.ItemViewModelFactory
-import com.example.weatherapp.ui.viewmodel.MainViewModel
 import com.example.weatherapp.ui.viewmodel.WeatherViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val dao = AppDatabase.getDatabase(application).itemDao()
-        val factory = ItemViewModelFactory(dao)
 
         val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
 
@@ -55,48 +46,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun App(application: Context) {
-    val navController = rememberNavController()
-    val bottomItems = listOf(BottomNavItem.Home,  BottomNavItem.Add, BottomNavItem.Others)
-
-    val weatherViewModel: WeatherViewModel = viewModel()
-
-    Scaffold(
-        bottomBar = { BottomBar(navController, bottomItems) }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = Routes.HOME,
-            modifier = androidx.compose.ui.Modifier.padding(innerPadding)
-        ) {
-
-            composable(Routes.OTHERS) {
-                val vm: MainViewModel = viewModel()
-                HomeScreen(viewModel = vm, onItemClick = { id ->
-                    navController.navigate(Routes.detailRoute(id))
-                })
-            }
-
-
-            composable(Routes.HOME) {
-                HomeScreen2(weatherViewModel)
-            }
-
-
-            composable(Routes.ADD) {
-                val dao = AppDatabase.getDatabase(application).itemDao()
-                val factory = ItemViewModelFactory(dao)
-                val viewModel: ItemViewModel = viewModel(factory = factory)
-                SavedDaoScreen(viewModel) }
-            composable(
-                route = Routes.DETAIL,
-                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
-            ) { backStackEntry ->
-                val vm: MainViewModel = viewModel()
-                val id = backStackEntry.arguments?.getInt("itemId") ?: -1
-                DetailScreen(itemId = id, viewModel = vm, onBack = { navController.popBackStack() })
-            }
-        }
-    }
-}
--- app/src/main/java/com/example/weatherapp/data/remote/model/FormAddState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.data.remote.model
-
-data class FormAddState (
-    val name: String = "",
-    val nameError: String? = null,
-    val description: String = "",
-    val descriptionError: String? = null,
-    val isValid: Boolean = false
-)
